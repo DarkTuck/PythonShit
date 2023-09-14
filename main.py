3f96ba52201ff018ae28a4269117622a27ecde51
DarkTuck <98091387+DarkTuck@users.noreply.github.com>
2023-09-14 15:49:21 +0200

    drobna poprawka

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import math
-def count (x1,x2,y1,y2):
-    a=(pow(x1+x2,2)+pow(y1+y2,2))
+def CountLenght (x1,x2,y1,y2):
+    a=(pow(x1-x2,2)+pow(y1-y2,2))
     b=math.sqrt(a)
     return(f"Długość odcinka to √{a} czyli {b}")
 print("Obliczanie długości odcinka \nwartości podaj bez nawasiu zamiast przecinka użyj spacji")
 x,y=input("punkt a\n").split()
 a,b=input("punkt b\n").split()
-print(count(int(x),int(a),int(y),int(b)))
+print(CountLenght(int(x),int(a),int(y),int(b)))
